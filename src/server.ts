f7b123e7c0798c3d3b654571524be8e86d313d34
eurico77 <euricomagalhaes61@gmail.com>
2020-08-08 11:37:41 -0300

    continuando o projeto

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express();
+app.use(json())
 
-app.listen(3333, () =>{
-  console.log('runing..');
-  
+app.listen(3333, () => {
+  console.log('runing...');
 })
+
+export { app }
