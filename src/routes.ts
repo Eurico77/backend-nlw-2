4c8b2b015ee1a7b3aacc83e4ed3785fddcc70c9e
eurico77 <euricomagalhaes61@gmail.com>
2020-08-09 15:27:35 -0300

    ok

--- src/routes.ts
+++ src/routes.ts
@@ -1,15 +1,35 @@
-
 import { Router } from "express";
+import { db } from "./database/connection";
 
 const routes = Router()
 
-routes.get('/', (req, res) => {
-  const users = [
-    { name: 'eurico', idade: 22 },
-    { name: 'chicão', idade: 18 },
-    { name: 'itao', idade: 25 }
+routes.post('/classes', async (req, res) => {
 
-  ]
-  return res.json(users)
+  const {
+    name,
+    avatar,
+    whatsapp,
+    bio,
+    subject,
+    cost,
+    schedule
+  } = req.body;
+
+  const insertUsersIds = await db('users').insert({
+    name,
+    avatar,
+    whatsapp,
+    bio
+  });
+
+  const user_id = insertUsersIds[0];
+
+  await db('classes').insert({
+    subject,
+    cost,
+    user_id
+  })
+
+  return res.send()
 })
 export { routes }
